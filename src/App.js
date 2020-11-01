b9b9b1e202cc597bd7dba2ad9f38eb08579b6c9f
MorrisOmbiro <morriskalb@gmail.com>
2020-10-31 20:58:09 -0500

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-export default function App() {
-  return (
-    <div>Hello Restart!</div>
-  );
+import Navbar from "./components/layout/navbar";
+import Landing from "./components/layout/landing-page";
+import Register from "./components/auth/register";
+import Login from "./components/auth/login";
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
+    );
+  }
 }
